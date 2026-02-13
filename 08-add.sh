0811c3b0c9e8d7b2f2f3353fd86f810274cc2a57
saisingala <saisingala@gmail.com>
2026-02-13 19:50:30 +0530

    adding numbers and date

--- 08-add.sh
+++ 08-add.sh
@@ -2,11 +2,6 @@
 date 
 a=$1
 b=$2
-add=` expr $a + $b `
-echo "add= $add"
-subtract=` expr $a - $b `
-echo "Sub= $subtract"
-Multiply=` expr $a*$b `
-echo "Multiply= $Multiply"
-Division=` expr $a / $b `
-echo "Div= $Division"
+add=(($a+$b))
+echo "$a and $b is = $add"
+
