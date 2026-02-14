7ec3979aa5b4683e2e2195c25f90012784224d48
saisingala <saisingala@gmail.com>
2026-02-14 06:55:46 +0530

    adding numbers and date

--- 11-condition.sh
+++ 11-condition.sh
@@ -5,4 +5,13 @@ then
 echo "Please run the script with root privilages"
 exit 1
 fi
-dnf install git
+
+dnf list installed git
+if [ $? -ne 0 ]; then
+     echo "Installing git"
+     dnf install git -y
+else 
+    echo " Git is already installed nothing to do..."
+fi
+
+
