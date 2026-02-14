b2d7493e3b76eec8be50739eac809cf2e25c70b4
saisingala <saisingala@gmail.com>
2026-02-14 07:25:43 +0530

    adding numbers and date

--- 11-condition.sh
+++ 11-condition.sh
@@ -1,23 +1,38 @@
 #!bin/bash
 USERID=$(id -u)
 if [ $USERID -ne 0 ];
-then
-echo "Please run the script with root privilages"
-exit 1
+ then
+   echo "Please run the script with root privilages"
+   exit 1
 fi
 
-dnf list installed git
+# dnf list installed git
+# if [ $? -ne 0 ]; then
+#      echo "Installing git"
+#      dnf install git -y
+#    if [ $? -ne 0 ]; then
+#          echo "git installation is not success. check it"
+#          exit 1
+#    else
+#          echo "git installation success" 
+#     fi
+# else 
+#     echo " Git is already installed nothing to do..."
+# fi
+
+dnf list installed mysql
 if [ $? -ne 0 ]; then
-     echo "Installing git"
-     dnf install git -y
-   if [ $? -ne 0 ]; then
-         echo "git installation is not success. check it"
-         exit 1
-   else
-         echo "git installation success" 
-    fi
-else 
-    echo " Git is already installed nothing to do..."
+   echo "Mysql is not installed.. going to install it"
+   dnf install mysqld -y
+  if [ $? -ne 0 ]; then
+      echo "Installation is not success. check it"
+      exit 1
+  else
+      echo "Mysql installation is success"
+   fi
+else
+  echo "mysql is already installed"
 fi
 
 
+   
