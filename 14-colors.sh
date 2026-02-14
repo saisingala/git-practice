eef8981482f9406645cd3abcbf155bb16e9b36ae
saisingala <saisingala@gmail.com>
2026-02-14 08:37:08 +0530

    adding numbers and date

--- 14-colors.sh
+++ 14-colors.sh
@@ -16,7 +16,7 @@ CHECK_ROOT(){
 VALIDATE (){
    
     if [ $1 -ne 0 ]; then
-        echo -e" $2 is ... $R Failed $N"
+        echo -e " $2 is ... $R Failed $N"
         exit 1
     else 
         echo -e "$2 is .... $G Success $N"
