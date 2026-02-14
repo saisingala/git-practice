d2fe0433cbf93f806529c365b9a7de462bf007f2
saisingala <saisingala@gmail.com>
2026-02-14 08:31:09 +0530

    adding numbers and date

---
+++ 14-colors.sh
@@ -0,0 +1,52 @@
+#!bin/bash
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ];
+      then
+      echo "Please run the script with root privilages"
+      exit 1
+    fi
+}
+
+VALIDATE (){
+   
+    if [ $1 -ne 0 ]; then
+        echo -e" $2 is ... $R Failed $N"
+        exit 1
+    else 
+        echo -e "$2 is .... $G Success $N"
+    fi        
+}
+
+CHECK_ROOT
+
+dnf list installed git
+
+
+if [ $? -ne 0 ]; then
+     echo "Installing git"
+     dnf install git -y
+
+     VALIDATE $? "Listing GIT "
+else 
+    echo " Git is already installed nothing to do..."
+fi
+ 
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+   echo "Mysql is not installed.. going to install it"
+   dnf install mysql -y
+   VALIDATE $? "Listing MYSQL "
+else
+  echo "mysql is already installed"
+fi
+
+
+   
+
