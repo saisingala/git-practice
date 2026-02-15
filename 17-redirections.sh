403ff15018bc222614b70f34e858188a3260c9e5
saisingala <saisingala@gmail.com>
2026-02-15 07:42:44 +0530

    adding numbers and date

--- 17-redirections.sh
+++ 17-redirections.sh
@@ -2,7 +2,7 @@
 LOGS_FOLDER=/var/log/shell-script
 SCRIPT_NAME=$(echo $0|cut -d "." -f1)
 TIMESTAMP=$(date +%d-%m-%Y-%H-%M-%S)
-LOG_FILE=$($LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.LOG)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
