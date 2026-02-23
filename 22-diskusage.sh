cf0167bf73a2916d5bc44ed0d537a1c109eb7de4
saisingala <saisingala@gmail.com>
2026-02-23 17:51:57 +0530

    MySql Shell Scirpt

--- 22-diskusage.sh
+++ 22-diskusage.sh
@@ -4,6 +4,11 @@ DISK_THRESHOLD=5 #IN REAL PROJECTS IT IS MORE THAN 755
 
 while IFS= read -r file # IFS internal field separator, empty it will ignore while space. -r for not  to ignore special characters like /#
 do
-   echo "Deleting file : $file"
-   
+   USAGE=$( echo $file|grep xfs|awk -F " " '{print $6F}'|cut -d  "%" -f1)
+   PARTITION=$(echo $file|grep xfs|awk -F " " '{print $NF}')
+
+   if [ $USAGE -gt $DISK_THRESHOLD ]
+   then 
+       echo "$PARTITION is more than $DISK_THRESHOLD, current value is $USAGE.. Please check and clear"
+   fi
 done <<< $DISK_USAGE
