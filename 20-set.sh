e9d0ebf642f162ea650100546a9b87ec479e7149
saisingala <saisingala@gmail.com>
2026-02-23 11:07:33 +0530

    MySql Shell Scirpt

--- 20-set.sh
+++ 20-set.sh
@@ -1,6 +1,11 @@
 #!bin/bash
 
 set -ex # setting the automatic exit  if we get any error e=exit, x=debug
+
+failure (){
+   echo "Failure at $1:$2"
+}
+trap 'failure "$LINENO" "$BASH_COOMAND"' ERR # ERR is the error signal
 echo "Success"
 echooo "FAILURE"
 Echo "failure"
