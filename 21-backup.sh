a149e4b87874816fd86d0a4def98943198eddc59
saisingala <saisingala@gmail.com>
2026-02-23 12:02:56 +0530

    MySql Shell Scirpt

---
+++ 21-backup.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+SRC_DIR=$1
+DEST_DIR=$2
+DAYS=${3:14} #this is optional if user provides ok otherwise it will take 14 days
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+USAGE(){
+    echo -e "$G Usage $Y <Source> <Destination> <DAYS optional>"
+}
+
+if[ $# -lt 2 ]
+then
+    USAGE
+fi
